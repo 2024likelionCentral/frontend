51d1ad03e97451f3eeb893a8abe97500f5582eda
yuchae Park <20232293@sungshin.ac.kr>
2024-07-23 21:00:44 +0900

    chore: import ReactDOM fix

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
 import './assets/scss/style.scss'
-import Header from './components/goal/Header';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    
   </React.StrictMode>
 );
