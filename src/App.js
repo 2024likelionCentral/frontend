c4608201ac7724578afb89007fd2e3fc3d1377aa
yuchae Park <20232293@sungshin.ac.kr>
2024-07-23 20:59:35 +0900

    feat: router-dom init

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Main from './pages/main/Main';
+import Login from './pages/login/Login';
+import Enter from './pages/enter/Enter';
 import Signin from './pages/signin/Signin';
 
 function App() {
   return (
-    <div className="App">
-      <Signin/>
-    </div>
+     <Router>
+      <div>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/enter" element={<Enter />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/main" element={<Main />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
